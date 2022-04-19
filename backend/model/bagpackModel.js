8ab7de92994b87b6ca868d7b0639c9f36f98c61e
Michael Solomon <michicko81@gmail.com>
2022-04-19 18:30:45 +0100

    added bagpack model

---
+++ backend/model/bagpackModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const slugify = require("slugify");
+
+const bagpackSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		unique: true,
+		required: [true, "A bagpack must have a name"],
+	},
+	materials: {
+		type: String,
+		required: [true, "A bagpack must have materials"],
+	},
+	images: {
+		type: [mongoose.Schema.Types.Mixed],
+		required: [true, "A bagpack must have images"],
+	},
+	size: {
+		type: String,
+		required: [true, "A bagpack must have a size"],
+	},
+	price: {
+		type: Number,
+		required: [true, "A bagpack must have a price"],
+	},
+	featured: {
+		type: Boolean,
+		default: false,
+		required: [true, "A bagpack must have a feature"],
+	},
+	slug: {
+		type: String,
+	},
+});
+
+bagpackSchema.pre('save', function (next) {
+	this.slug = slugify(this.name);
+	next();
+});
+
+const Bagpack = mongoose.model('Bagpack', bagpackSchema);
+module.exports = Bagpack;
