5b49bc178c54af5689067d7d872569044ef56670
Michael Solomon <michicko81@gmail.com>
2022-05-03 11:01:02 +0100

    redo cart component

---
+++ backend/client/src/utils/cartitems.js
@@ -0,0 +1,37 @@
+module.exports = [
+	{
+		name: "058-2014",
+		material: " canvas, leather",
+		size: "56X50X23cm",
+		stock: 2,
+		price: 230,
+	},
+	{
+		name: "069-2018",
+		material: " canvas, leather",
+		size: "46X45X30cm",
+		stock: 3,
+		price: 250,
+	},
+	{
+		name: "072-2018",
+		material: " canvas, leather",
+		size: "25X50X25cm",
+		stock: 1,
+		price: 170,
+	},
+	{
+		name: "075-2019",
+		material: " canvas, leather",
+		size: "36X35X26cm",
+		stock: 2,
+		price: 250,
+	},
+	{
+		name: "062-2015",
+		material: " canvas, leather",
+		size: "36X40X23cm",
+		stock: 4,
+		price: 320,
+	}
+];
