63691328ca48d3f3187b93ef009efc1046388585
Michael Solomon <michicko81@gmail.com>
2022-05-13 12:56:07 +0100

    handled the contact formm

--- backend/client/src/components/contact/Contact.js
+++ backend/client/src/components/contact/Contact.js
@@ -1,21 +1,47 @@
-import ContactForm from "./ContactForm"
-
+import { useEffect, useState } from "react";
+import ContactForm from "./ContactForm";
 
 const Contact = () => {
-  return (
-		<section className='contact-section' id="contact">
+	const [messageSent, setMessageSent] = useState(false);
+	const [message, setMessage] = useState("");
+	const [submitBtnDisabled, setSubmitBtnDisabled] = useState(false);
+
+	useEffect(() => {
+		if (messageSent) {
+			const timer = setTimeout(() => {
+				setMessageSent(false);
+				setMessage("");
+				setSubmitBtnDisabled(false)
+			}, 7000);
+
+			// clear timeout
+
+			return () => {
+				clearTimeout(timer);
+			};
+		}
+	}, [messageSent]);
+
+	return (
+		<section className='contact-section' id='contact'>
 			<div className='container'>
 				<div className='contact-content'>
 					<h2 className='secondary-heading pg-heading'>Contact us</h2>
-          <div className='contact-form-container'>
-            <h4>Please leave a message</h4>
-            <p>We'll get back to you soon.</p>
-            <ContactForm/>
-          </div>
+					<div className='contact-form-container'>
+						<h4>Please leave a message</h4>
+						<p>We'll get back to you soon.</p>
+						<ContactForm
+							setMessage={setMessage}
+							setMessageSent={setMessageSent}
+							submitBtnDisabled={submitBtnDisabled}
+						setSubmitBtnDisabled={setSubmitBtnDisabled}
+						/>
+						{messageSent && <p className='contact-alert'>{message}</p>}
+					</div>
 				</div>
 			</div>
 		</section>
 	);
-}
- 
+};
+
 export default Contact;
